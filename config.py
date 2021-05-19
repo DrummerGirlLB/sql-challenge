27ccf18d2842a73c1838b5c59137a65dbcd5cb07
Lora Brown <drummergirllb@hotmail.com>
2021-05-19 08:28:56 -0700

    update config file

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 secretnumber = "removed because doesnt work"
+I tried and tried to get this to work and couldnt without using my actual password in the file. 
+I wanted to research but I just ran out of time
